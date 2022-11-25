c1815038cd77e57523e8e93877642275f686d395
Mark Boyd <mark.boyd@gsa.gov>
2022-11-25 16:46:42 -0500

    rename script

--- scripts/get-apps-count-by-type.sh
+++ scripts/get-apps-count-by-type.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-
-# TOTAL_PAGES=$(cf curl '/v3/apps' | jq -r '.pagination.total_pages')
-TOTAL_PAGES=3
+TOTAL_PAGES=$(cf curl '/v3/apps' | jq -r '.pagination.total_pages')
 
 DOCKER_COUNT=0
 DOTNET_COUNT=0
 APT_COUNT=0
+BINARY_COUNT=0
 GO_COUNT=0
 NGINX_COUNT=0
 NODEJS_COUNT=0
 JAVA_COUNT=0
 PHP_COUNT=0
 PYTHON_COUNT=0
+R_COUNT=0
 RUBY_COUNT=0
 STATICFILE_COUNT=1
 
 for ((i=1;i <=TOTAL_PAGES ;i++)); do
-    echo "page: $i"
-
     while IFS= read -r resource; do
         LIFECYCLE_TYPE=$(echo "$resource" | jq -r '.lifecycle.type')
         if [[ $LIFECYCLE_TYPE == "docker" ]]; then
@@ -28,6 +26,10 @@ for ((i=1;i <=TOTAL_PAGES ;i++)); do
                 case $buildpack in
                     "apt_buildpack" | *apt-buildpack*)
                         APT_COUNT=$(( APT_COUNT + 1 ))
+                    ;;
+
+                    "binary_buildpack" | *binary-buildpack*)
+                        BINARY_COUNT=$(( BINARY_COUNT + 1 ))
                     ;;
 
                     "dotnet_core_buildpack" | *dotnet-core-buildpack*)
@@ -58,11 +60,15 @@ for ((i=1;i <=TOTAL_PAGES ;i++)); do
                         PYTHON_COUNT=$(( PYTHON_COUNT + 1 ))
                     ;;
 
+                    "r_buildpack" | *r-buildpack*)
+                        R_COUNT=$(( R_COUNT + 1 ))
+                    ;;
+
                     "ruby_buildpack" | *ruby-buildpack*)
                         RUBY_COUNT=$(( RUBY_COUNT + 1 ))
                     ;;
 
-                    "staticfile_buildpack")
+                    "staticfile_buildpack" | *staticfile-buildpack*)
                         STATICFILE_COUNT=$(( STATICFILE_COUNT + 1 ))
                     ;;
                         
@@ -77,12 +83,14 @@ done;
 
 echo "total docker apps: $DOCKER_COUNT"
 echo "total APT apps: $APT_COUNT"
+echo "total Binary apps: $BINARY_COUNT"
 echo "total .NET apps: $DOTNET_COUNT"
 echo "total Go apps: $GO_COUNT"
 echo "total Java apps: $JAVA_COUNT"
 echo "total nodeJS apps: $NODEJS_COUNT"
 echo "total Nginx apps: $NGINX_COUNT"
 echo "total PHP apps: $PHP_COUNT"
+echo "total R apps: $R_COUNT"
 echo "total Ruby apps: $RUBY_COUNT"
 echo "total Python apps: $PYTHON_COUNT"
 echo "total static apps: $STATICFILE_COUNT"
